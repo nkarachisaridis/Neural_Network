65681d3b787cba9ded77b150458ee4f408da59fe
nKarahisaridiss <n.karahisaridis@gmail.com>
2022-12-04 18:37:17 +0200

    changes

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('aa')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from keras.preprocessing.image import ImageDataGenerator , load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import os
