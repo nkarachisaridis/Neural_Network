566472d57a43094bdd241edfd4e7dc3c29272961
nKarahisaridiss <n.karahisaridis@gmail.com>
2022-12-05 20:04:11 +0200

    changes

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from keras.preprocessing.image import ImageDataGenerator , load_img
 from tensorflow.keras.utils import to_categorical
