c447bdcedf0479339d0910040775d35b9f9227bb
nKarahisaridiss <n.karahisaridis@gmail.com>
2022-12-06 16:54:15 +0200

    change2

--- test.py
+++ test.py
@@ -1,96 +1,195 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-
+import pandas as pd 
 from keras.preprocessing.image import ImageDataGenerator 
-#from tensorflow.python.keras.utils import to_categorical
+from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
 import os
-from keras_preprocessing.image import load_img
+import PIL.Image
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-from keras.layers import GlobalAveragePooling2D
-from keras.models import Model
-import tensorflow
-from  tensorflow.python.keras.models import Sequential
-from  tensorflow.python.keras.layers import Conv2D , MaxPooling2D , Dropout , Flatten , Dense , Activation
-from keras.optimizers import adam_v2
+print(os.listdir("../Desktop/mixed"))
 
-Image_width = 128
-Image_height = 128
-Image_size = (Image_width , Image_height)
-Image_channel = 3
+FAST_RUN = False
+IMAGE_WIDTH=128
+IMAGE_HEIGHT=128
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
 
-Image_rgb_size = (Image_width ,Image_height , 3 )
 
-filenames = os.listdir('../Desktop/mixed/TRAIN')
-categories = []
+filenames = os.listdir("../Desktop/mixed/TRAIN")
+ids = []
 for filename in filenames:
-    category = filename.split('.')[0]
-    if category == 'al':
-        categories.append(1)
+    id = filename.split('.')[0]
+    if id == 'al':
+        ids.append(1)
     else:
-        categories.append(0)
-data = pd.DataFrame({'filename' : filenames , 'category' : categories})
+        ids.append(0)
 
-#print(data['category'].value_counts())
+df = pd.DataFrame({
+    'filename': filenames,
+    'id': ids
+})
 
-data['category1'] = data['category'].apply(lambda x: 'tumor' if x== 0 else 'alzheimer')
-#print(data.head())
+df["category"] = df["id"].replace({0: 'tumor', 1: 'alzheimer'}) 
 
-data.drop(['category'] , inplace = True , axis = 1)
-data.rename(columns  = {'category1' : 'category'} , inplace =True)
-#print(data.head())
+print(df.head())
+#print(df.tail())
 
 
-train_df , val_df = train_test_split(data , test_size = 0.2 , random_state = 42)
-train_df = train_df.reset_index(drop = True)
-val_df = val_df.reset_index(drop = True)
+model = Sequential()
 
-#print(train_df.shape , val_df.shape)
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-batch_size  = 15
-epochs = 7 #due to speed
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+model.summary()
+
+
+earlystop = EarlyStopping(patience=10)
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                            patience=2, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+
+callbacks = [earlystop, learning_rate_reduction]
+
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+train_df['category'].value_counts().plot.bar()
+plt.show()
+
 total_train = train_df.shape[0]
-total_validate = val_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=15
 
-train_dategen = ImageDataGenerator(rotation_range = 15 , 
-                                  rescale = 1.0/255 ,
-                                  shear_range = 0.1,
-                                  zoom_range = 0.2 , 
-                                  horizontal_flip = True , 
-                                  width_shift_range = 0.1 , 
-                                  height_shift_range = 0.1
-                                  )
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
 
-train_generator = train_dategen.flow_from_dataframe(train_df, 
-                                                    "../Desktop/mixed/TRAIN", 
-                                                    x_col='filename',
-                                                    y_col='category',
-                                                    target_size=Image_size,
-                                                    class_mode='categorical',
-                                                    batch_size=batch_size
-                                                    ) 
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "../Desktop/mixed/TRAIN/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df, 
+    "../Desktop/mixed/TRAIN/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+example_df = train_df.sample(n=1).reset_index(drop=True)
+example_generator = train_datagen.flow_from_dataframe(
+    example_df, 
+    "../Desktop/mixed/TRAIN/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical'
+)
 
-validation_datagen = ImageDataGenerator(rescale = 1./255)
-val_generator = validation_datagen.flow_from_dataframe(val_df, 
-                                                        "../Desktop/mixed/TRAIN", 
-                                                        x_col='filename',
-                                                        y_col='category',
-                                                        target_size=Image_size,
-                                                        class_mode='categorical',
-                                                        batch_size=batch_size
-                                                        )  
+plt.figure(figsize=(12, 12))
+for i in range(0, 15):
+    plt.subplot(5, 3, i+1)
+    for X_batch, Y_batch in example_generator:
+        image = X_batch[0]
+        plt.imshow(image)
+        break
+plt.tight_layout()
+plt.show()
 
 
-base = tensorflow.keras.applications.resnet50.ResNet50(weights = 'imagenet' , include_top = False , input_shape = Image_rgb_size)
+epochs=3 if FAST_RUN else 10
+history = model.fit_generator(
+    train_generator, 
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
 
 
-x = base.output
-x = GlobalAveragePooling2D()(x)
-x = Dropout(0.7)(x)
-prediction = Dense(2,activation='softmax')(x)
-model = Model(inputs = base.input, outputs = prediction)
-adam = adam_v2.Adam(learning_rate=0.0001)
-model.compile(optimizer= adam, loss='categorical_crossentropy', metrics=['accuracy'])
+model.save_weights("model.h5")
+
+'''
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+ax1.plot(history.history['loss'], color='b', label="Training loss")
+ax1.plot(history.history['val_loss'], color='r', label="validation loss")
+ax1.set_xticks(np.arange(1, epochs, 1))
+ax1.set_yticks(np.arange(0, 1, 0.1))
+
+ax2.plot(history.history['accuracy'], color='b', label="Training accuracy")
+ax2.plot(history.history['val_accuracy'], color='r',label="Validation accuracy")
+ax2.set_xticks(np.arange(1, epochs, 1))
+
+legend = plt.legend(loc='best', shadow=True)
+plt.tight_layout()
+plt.show()
+
+'''
+
+test_filenames = os.listdir("../input/test1/test1")
+test_df = pd.DataFrame({
+    'filename': test_filenames
+})
+nb_samples = test_df.shape[0]
+
+test_gen = ImageDataGenerator(rescale=1./255)
+test_generator = test_gen.flow_from_dataframe(
+    test_df, 
+    "../input/test1/test1/", 
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    target_size=IMAGE_SIZE,
+    batch_size=batch_size,
+    shuffle=False
+)
+
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))
+
+test_df['category'] = np.argmax(predict, axis=-1)
+
+label_map = dict((v,k) for k,v in train_generator.class_indices.items())
+test_df['category'] = test_df['category'].replace(label_map)
+test_df['category'] = test_df['category'].replace({ 'dog': 1, 'cat': 0 })
